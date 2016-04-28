45e6709026a3e51888d27ef504d5790e535e5e09
anuraagbasu <anuraag@wooplr.com>
2016-04-28 15:32:49 +0530

    Configured project for heroku

--- GreedyMusic/settings.py
+++ GreedyMusic/settings.py
@@ -140,14 +140,14 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'https://greedy-music.herokuapp.com/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
