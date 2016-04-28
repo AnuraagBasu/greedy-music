f01e713ea6ed71a0419347f61efc3a3497cdb0c9
anuraagbasu <anuraag@wooplr.com>
2016-04-28 14:00:20 +0530

    Configured project for heroku

--- GreedyMusic/urls.py
+++ GreedyMusic/urls.py
@@ -1,6 +1,6 @@
 __author__ = 'anuraagbasu'
 
-from django.conf.urls.defaults import include, url, patterns
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
