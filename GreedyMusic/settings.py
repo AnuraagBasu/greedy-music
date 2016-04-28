2a159eb6212b00f183fde43af8decbf74a780c8b
anuraagbasu <anuraag@wooplr.com>
2016-04-28 15:19:46 +0530

    Configured project for heroku

--- GreedyMusic/settings.py
+++ GreedyMusic/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
